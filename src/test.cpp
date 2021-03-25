a7d62b2d1b6b02b1851726936faea72a6bfe88cc
ktchow1 <ktchow1@yahoo.com.hk>
2021-03-26 00:45:19 +0800

    epoll socket downloaded from web

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 void test_ipc_unnamed_pipe();
 void test_ipc_named_pipe(bool);
 void test_ipc_shared_memory();
+void test_socket(int argc, char *argv[]);
 
 
 int main(int argc, char* argv[])
@@ -10,6 +11,7 @@ int main(int argc, char* argv[])
 //  test_ipc_unnamed_pipe();
 //  test_ipc_named_pipe(argc > 1); // with argument for producer
     test_ipc_shared_memory();
+    test_socket(argc, argv);
 
     std::cout << "\n\n\n";
     return 0;
