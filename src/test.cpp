2f1feaaaeaea3185ac7d175c43d5b103f3bc3586
ktchow1 <ktchow1@yahoo.com.hk>
2021-03-26 01:04:26 +0800

    formatting

--- src/test.cpp
+++ src/test.cpp
@@ -8,9 +8,9 @@ void test_socket(int argc, char *argv[]);
 
 int main(int argc, char* argv[])
 { 
-//  test_ipc_unnamed_pipe();
-//  test_ipc_named_pipe(argc > 1); // with argument for producer
-    test_ipc_shared_memory();
+//  test_ipc_unnamed_pipe();   
+    test_ipc_named_pipe(argc > 1); // with argument for producer
+//  test_ipc_shared_memory(); 
     test_socket(argc, argv);
 
     std::cout << "\n\n\n";
