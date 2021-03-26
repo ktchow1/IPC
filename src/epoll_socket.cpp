4559228cf59b5824a1adc6d554db31f69c54df1e
ktchow1 <ktchow1@yahoo.com.hk>
2021-03-26 22:24:21 +0800

    epoll for socket

--- src/epoll_socket.cpp
+++ src/epoll_socket.cpp
@@ -1,232 +1,206 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/epoll.h>
-#include <errno.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define MAXEVENTS 64
 
-static int make_socket_non_blocking (int sfd)
+// ************** //
+// *** Step 1 *** //
+// ************** //
+int create_and_bind(std::uint16_t port)
 {
-    int flags, s;
+    addrinfo hints;
+    addrinfo *result;
 
-    flags = fcntl (sfd, F_GETFL, 0);
-    if (flags == -1)
+    // Step 1a. initial address hints
+    memset(&hints, 0, sizeof(addrinfo));
+    hints.ai_socktype = SOCK_STREAM; // request TCP socket
+    hints.ai_family   = AF_UNSPEC;   // request both IPv4 and IPv6
+    hints.ai_flags    = AI_PASSIVE;
+
+    // Step 1b. get address results
+    std::string port_str = std::to_string(port);
+    if (auto status = getaddrinfo(NULL, port_str.c_str(), &hints, &result) != 0) return -1;
+
+    // Step 1c. iterate address results
+    for(const auto* result_ptr = result; result_ptr!=NULL; result_ptr = result_ptr->ai_next)
     {
-        perror ("fcntl");
-        return -1;
+        int fd = socket(result_ptr->ai_family,
+                        result_ptr->ai_socktype,
+                        result_ptr->ai_protocol);
+
+        if (fd == -1) continue;
+        if (bind(fd, result_ptr->ai_addr,
+                     result_ptr->ai_addrlen) == 0)
+        {
+            freeaddrinfo(result);
+            return fd;
+        }
+        close(fd);
     }
 
+    freeaddrinfo(result);
+    return -1;
+}
+
+// ************** //
+// *** Step 2 *** //
+// ************** //
+int set_socket_non_blocking(int fd)
+{
+    // Get flags
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) return -1;
+
+    // Set flags
     flags |= O_NONBLOCK;
-    s = fcntl (sfd, F_SETFL, flags);
-    if (s == -1)
-    {
-        perror ("fcntl");
-        return -1;
-    }
+    if (fcntl(fd, F_SETFL, flags) == -1) return -1;
     return 0;
 }
 
-static int create_and_bind (char *port)
+// **************** //
+// *** Step 1-3 *** //
+// **************** //
+int create_passive_socket(std::uint16_t port)
 {
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int s, sfd;
+    int FD_passive = create_and_bind(port);
+    if (FD_passive == -1)                          return -1;
+    if (set_socket_non_blocking(FD_passive) == -1) return -1;
+    if (listen(FD_passive, SOMAXCONN) == -1)       return -1;
 
-    memset (&hints, 0, sizeof (struct addrinfo));
-    hints.ai_family = AF_UNSPEC;     /* Return IPv4 and IPv6 choices */
-    hints.ai_socktype = SOCK_STREAM; /* We want a TCP socket */
-    hints.ai_flags = AI_PASSIVE;     /* All interfaces */
-
-    s = getaddrinfo (NULL, port, &hints, &result);
-    if (s != 0)
-    {
-        fprintf (stderr, "getaddrinfo: %s\n", gai_strerror (s));
-        return -1;
-    }
-   
-    for (rp = result; rp != NULL; rp = rp->ai_next)
-    {
-        sfd = socket (rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        if (sfd == -1) continue;
-   
-        s = bind (sfd, rp->ai_addr, rp->ai_addrlen);
-        if (s == 0) break;
-        close (sfd);
-    }
-   
-    if (rp == NULL)
-    {
-        fprintf (stderr, "Could not bind\n");
-        return -1;
-    }
-    freeaddrinfo (result);
-    return sfd;
+    return FD_passive;
 }
 
-void test_socket(int argc, char *argv[])
+// *************** //
+// *** Helpers *** //
+// *************** //
+std::string get_client_ip(const sockaddr& addr, const socklen_t& len)
 {
-     int sfd, s;
-     int efd;
-     struct epoll_event event;
-     struct epoll_event *events;
-    
-     if (argc != 2)
-     {
-         fprintf (stderr, "Usage: %s [port]\n", argv[0]);
-         exit (EXIT_FAILURE);
-     }
-    
-     sfd = create_and_bind (argv[1]);
-     if (sfd == -1) abort ();
-    
-     s = make_socket_non_blocking (sfd);
-     if (s == -1) abort ();
-    
-     s = listen (sfd, SOMAXCONN);
-     if (s == -1)
-     {
-         perror ("listen");
-         abort ();
-     }
-    
-     efd = epoll_create1 (0);
-     if (efd == -1)
-     {
-         perror ("epoll_create");
-         abort ();
-     }
-    
-     event.data.fd = sfd;
-     event.events = EPOLLIN | EPOLLET;
-     s = epoll_ctl (efd, EPOLL_CTL_ADD, sfd, &event);
-     if (s == -1)
-     {
-         perror ("epoll_ctl");
-         abort ();
-     }
-     events = (epoll_event*) calloc (MAXEVENTS, sizeof event);
-    
-     // ********************** //
-     // *** The event loop *** //
-     // ********************** //
-     while (1)
-     {
-         int n, i;
-    
-         n = epoll_wait (efd, events, MAXEVENTS, -1);
-         for (i = 0; i < n; i++)
-         {
-             if ((events[i].events & EPOLLERR) ||
-                 (events[i].events & EPOLLHUP) ||
-                 (!(events[i].events & EPOLLIN)))
-             {
-                 // An error has occured on this fd, or the socket is not ready for reading.
-                 fprintf (stderr, "epoll error\n");
-                 close (events[i].data.fd);
-                 continue;
-             }
-             else if (sfd == events[i].data.fd)
-             {
-                 while (1)
-                 {
-                     struct sockaddr in_addr;
-                     socklen_t in_len;
-                     int infd;
-                     char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
-    
-                     in_len = sizeof in_addr;
-                     infd = accept (sfd, &in_addr, &in_len);
-                     if (infd == -1)
-                     {
-                         if ((errno == EAGAIN) || (errno == EWOULDBLOCK))
-                         {
-                             /* We have processed all incoming
-                                connections. */
-                             break;
-                         }
-                         else
-                         {
-                             perror ("accept");
-                             break;
-                         }
-                     }
-    
-                     s = getnameinfo (&in_addr, in_len,
-                                      hbuf, sizeof hbuf,
-                                      sbuf, sizeof sbuf,
-                                      NI_NUMERICHOST | NI_NUMERICSERV);
-                     if (s == 0)
-                     {
-                         printf("Accepted connection on descriptor %d (host=%s, port=%s)\n", infd, hbuf, sbuf);
-                     }
-    
-                     // Make the incoming socket non-blocking and add it to list of fds to monitor. 
-                     s = make_socket_non_blocking (infd);
-                     if (s == -1) abort ();
-    
-                     event.data.fd = infd;
-                     event.events = EPOLLIN | EPOLLET;
-                     s = epoll_ctl (efd, EPOLL_CTL_ADD, infd, &event);
-                     if (s == -1)
-                     {
-                         perror ("epoll_ctl");
-                         abort ();
-                     }
-                 }
-                 continue;
-             }
-             else
-             {
-                 int done = 0;
-                 while (1)
-                 {
-                     ssize_t count;
-                     char buf[512];
-    
-                     count = read (events[i].data.fd, buf, sizeof buf);
-                     if (count == -1)
-                     {
-                         // If errno == EAGAIN, that means we have read all data. So go back to the main loop. 
-                         if (errno != EAGAIN)
-                         {
-                             perror ("read");
-                             done = 1;
-                         }
-                         break;
-                     }
-                     else if (count == 0)
-                     {
-                         // End of file. The remote has closed the connection.
-                         done = 1;
-                         break;
-                     }
-    
-                     // Write the buffer to standard output 
-                     s = write (1, buf, count);
-                     if (s == -1)
-                     {
-                         perror ("write");
-                         abort ();
-                     }
-                 }
-    
-                 if (done)
-                 {
-                     printf ("Closed connection on descriptor %d\n", events[i].data.fd);
-    
-                     // Closing the descriptor will make epoll remove it
-                     // from the set of descriptors which are monitored. 
-                     close (events[i].data.fd);
-                 }
-             }
-         }
-     }
-    
-     free (events);
-     close (sfd);
+    char buf0[NI_MAXHOST];
+    char buf1[NI_MAXSERV];
+    std::string str;
+
+    if (getnameinfo(&addr, len, buf0, sizeof(buf0),
+                                buf1, sizeof(buf1),
+                                NI_NUMERICHOST | NI_NUMERICSERV) == 0)
+    {
+        using namespace std::string_literals;
+        str = std::string(buf0) + ":"s + std::string(buf1);
+    }
+    return str;
+}
+
+int register_fd_to_epoll(int FD_epoll, int FD)
+{
+    struct epoll_event event;
+    event.data.fd = FD;
+    event.events = EPOLLIN | EPOLLET;
+    return epoll_ctl(FD_epoll, EPOLL_CTL_ADD, FD, &event);
+}
+
+// *************** //
+// *** Testing *** //
+// *************** //
+void test_epoll_socket(std::uint16_t port)
+{
+    int FD_passive = create_passive_socket(port);
+    if (FD_passive == -1)
+    {
+        std::cout << "\nFail to create passive socket";
+        return;
+    }
+
+    int FD_epoll = epoll_create1(0);
+    if (FD_epoll == -1)
+    {
+        std::cout << "\nFail to create epoll";
+        return;
+    }
+
+    if (register_fd_to_epoll(FD_epoll, FD_passive) == -1)
+    {
+        std::cout << "\nFail to register fd to epoll";
+        return;
+    }
+
+    // ********************** //
+    // *** The event loop *** //
+    // ********************** //
+    epoll_event events[MAXEVENTS];
+    while(true)
+    {
+        // Blocked until ready ...
+        int num_events = epoll_wait(FD_epoll, events, MAXEVENTS, -1);
+        for(int n=0; n!=num_events; ++n)
+        {
+            // ******************************* //
+            // *** Event 1 : Disconnection *** //
+            // ******************************* //
+            if ((events[n].events & EPOLLERR) ||
+                (events[n].events & EPOLLHUP) ||
+               !(events[n].events & EPOLLIN ))
+            {
+                std::cout << "\nDisconnection detected";
+                close(events[n].data.fd);
+            }
+            // ******************************** //
+            // *** Event 2 : New connection *** //
+            // ******************************** //
+            else if (FD_passive == events[n].data.fd)
+            {
+                sockaddr sock_addr;
+                socklen_t sock_len = sizeof(sock_addr);
+
+                int FD_active = accept(FD_passive, &sock_addr, &sock_len);
+                if (FD_active == -1)
+                {
+                    std::cout << "\nError in acceptng new connection, next event ... ";
+                }
+                else
+                {
+                    std::cout << "\nAccepted connection from " << get_client_ip(sock_addr, sock_len);
+
+                    if (set_socket_non_blocking(FD_active) == -1)
+                    {
+                        std::cout << "\nFail to set socket non blocking";
+                        return;
+                    }
+
+                    if (register_fd_to_epoll(FD_epoll, FD_active) == -1)
+                    {
+                        std::cout << "\nFail to register fd to epoll";
+                        return;
+                    }
+                }
+            }
+            // ************************************************** //
+            // *** Event 3 : Message from existing connection *** //
+            // ************************************************** //
+            else
+            {
+                char buf[512];
+                size_t count = read(events[n].data.fd, buf, sizeof(buf));
+                if (count == -1)
+                {
+                    std::cout << "\nConnection read failure";
+                    close(events[n].data.fd);
+                    break;
+                }
+                else if (count == 0)
+                {
+                    std::cout << "\nDisconnection detected [EOF]";
+                    close(events[n].data.fd);
+                    break;
+                }
+
+                std::cout << "\n" << std::string(buf, count);
+                write(events[n].data.fd, buf, count); // echo back
+            }
+        }
+    }
+    close(FD_passive);
 }
--- src/test.cpp
+++ src/test.cpp
@@ -3,15 +3,25 @@
 void test_ipc_unnamed_pipe();
 void test_ipc_named_pipe(bool);
 void test_ipc_shared_memory();
-void test_socket(int argc, char *argv[]);
+void test_epoll_socket(std::uint16_t port);
 
 
 int main(int argc, char* argv[])
 { 
 //  test_ipc_unnamed_pipe();   
-    test_ipc_named_pipe(argc > 1); // with argument for producer
+//  test_ipc_named_pipe(argc > 1); // with argument for producer
 //  test_ipc_shared_memory(); 
-    test_socket(argc, argv);
+
+    {
+        if (argc!=2)
+        {
+            std::cout << "\nError in input, please enter : exe 12345";
+        }
+        else
+        {
+            test_epoll_socket(std::stoi(std::string(argv[1])));
+        }
+    }
 
     std::cout << "\n\n\n";
     return 0;
